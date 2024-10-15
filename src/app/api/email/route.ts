556aab5922ceb710f521d056973545f3eb36cafa
Gokul C <gokulc@Gokuls-MacBook-Air.local>
2024-10-15 17:16:43 +0530

    await func added

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,30 +1,26 @@
 import request from "request";
 import mailer from "./mailer";
 export async function POST(req: Request) {
-  let res = {
-    message: '',
-    type: ''
-  }
   const reqBody = await req.text();
   const eventData = JSON.stringify(reqBody);
-  mailer({ eventData }).then(() => {
-    res = { message: 'Message Sent Success', type: 'success' }
 
-  }).catch(() => {
-    res = { message: 'Message Sent Failed', type: 'error' }
-  })
+  try {
+    const res = await mailer({ eventData })
+    return Response.json(res)
+  } catch (error) {
+    return Response.json(error)
+  }
 
-  return Response.json(res)
 }
 
 export async function GET() {
   // const body = await req.text();
-  const event = { test: 'testevent'}
+  const event = { test: 'testevent' }
   request.post({
-    headers: {'content-type' : 'application/json'},
-    url:     'https://ses-email-logs.vercel.app',
-    body:    JSON.stringify(event)
-  }, function(error){
+    headers: { 'content-type': 'application/json' },
+    url: 'https://ses-email-logs.vercel.app',
+    body: JSON.stringify(event)
+  }, function (error) {
     console.log('error', error);
     // console.log('response', response);
     // console.log('body', body);
